6d9c78ce171fa1bb62fadb105ed88b593f21e7f0
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-11-25 15:02:57 +0300

    added file Homework4

---
+++ Homework4.py
@@ -0,0 +1,27 @@
+# Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+import math
+quarter = input('Введите номер координатной четверти: ')
+if (quarter.isdigit() == True):
+    quarter = int(quarter)
+    while (quarter < 5):
+        x_pos_end = math.inf
+        y_pos_end = math.inf
+        x_neg_end = -math.inf
+        y_neg_end = -math.inf
+        if (quarter == 1):
+            print(f'Диапазон возможных координат точки в 1-й четверти:\nx = {(0, x_pos_end)} и y = {(0, y_pos_end)}')
+            break
+        elif (quarter == 2):
+            print(f'Диапазон возможных координат точки во 2-й четверти:\nx = {(0, x_neg_end)} и y = {(0, y_pos_end)}')
+            break
+        elif (quarter == 3):
+            print(f'Диапазон возможных координат точки в 3-й четверти:\nx = {(0, x_neg_end)} и y = {(0, y_neg_end)}')
+            break
+        elif (quarter == 4):
+            print(f'Диапазон возможных координат точки в 4-й четверти:\nx = {(0, x_pos_end)} и y = {(0, y_neg_end)}')
+            break
+        else:
+            print(f'Четверти {quarter} не существует.')
+            break
+else:
+    print('Необходимо ввести число вместо символа')
