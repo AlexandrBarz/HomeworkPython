9f6432f40c6f2ad91762e4d4cce0331436e8740f
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-11-27 18:33:07 +0300

    corrected file Homework6

--- Homework6.py
+++ Homework6.py
@@ -1,20 +1,26 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр. Учтите, что числа могут быть отрицательными
+
+# Метод через строковую переменную
 # num = input('Введите число: ')
-# #if num.startswith('-') or num.isdigit():
 # sum = 0
 # for i in num:
 #     if (i != '.') and (i !=','):
 #         sum += int(i)
 # print(f'Сумма цифр введенного числа {num} = {sum}')
-# # else:
-#     # print('Необходимо ввести число вместо символа')
 
-num = float(input('Введите число: '))
-num1 = abs(num)
-sum = 0
-while num1 > 0:
-    res = num1 % 10
-    sum += res
-    num1 //= 10
-    print(num1)
-print(f'Сумма цифр введенного числа {num} = {sum}')
+
+# Метод через переменную float
+def sum_digit (x):
+    sum = 0
+    while x > 0:
+        res = x % 10
+        sum += res
+        x //= 10
+    return sum
+
+number = input('Введите число: ')
+number1 = abs(float(number))
+number2 = number1 - math.floor(number)
+number1 == math.floor(number)
+
+print(f'Сумма цифр введенного числа {number} = {sum_digit(number1)}')
