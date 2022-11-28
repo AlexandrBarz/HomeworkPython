75fffaba2788ea8a7231fa45f31318f2af0fdaf5
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-11-28 12:54:36 +0300

    corrected file Homework6

--- Homework6.py
+++ Homework6.py
@@ -1,26 +1,25 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр. Учтите, что числа могут быть отрицательными
 
 # Метод через строковую переменную
-# num = input('Введите число: ')
-# sum = 0
-# for i in num:
-#     if (i != '.') and (i !=','):
-#         sum += int(i)
-# print(f'Сумма цифр введенного числа {num} = {sum}')
+num = input('Введите число: ')
+sum = 0
+for i in num:
+    if (i != '.') and (i !=','):
+        sum += int(i)
+print(f'Сумма цифр введенного числа {num} = {sum}')
 
 
 # Метод через переменную float
-def sum_digit (x):
-    sum = 0
-    while x > 0:
-        res = x % 10
-        sum += res
-        x //= 10
-    return sum
-
-number = input('Введите число: ')
-number1 = abs(float(number))
-number2 = number1 - math.floor(number)
-number1 == math.floor(number)
-
-print(f'Сумма цифр введенного числа {number} = {sum_digit(number1)}')
+num = input('Введите число: ')
+num1 = abs(int(num))
+num2 = abs(num) - num1
+sum = 0
+while num1 != 0:
+    res = num1 % 10
+    sum += res
+    num1 //= 10
+while num2 != 0:
+    sum += int(num2*10)
+    num2 = num2*10 - int(num2*10)
+    num2 = round(num2, 10)  # для сокращения знаков после запятой. Без нее программа тоже работает
+print(f'Сумма цифр введенного числа {num} = {sum}')
