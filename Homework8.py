fe1e6706eed0223f079a0d8b982560b57b4066e6
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-11-28 21:18:39 +0300

    corrected file Homework8

--- Homework8.py
+++ Homework8.py
@@ -1,14 +1,17 @@
 # Дан массив размера N. После каждого отрицательного элемента массива вставьте элемент с нулевым значением.
 import random
-n = int(input('Введите размер массива: '))
-numbers = []
-for i in range(n):
-    numbers.append(random.randint(-100,100))
-print(f'Исходный массив:\n{numbers}')
-new_numbers = numbers.copy()
-count = -1
-for x in range(len(numbers)):
-    if numbers[x] < 0:
-        count +=1
-        new_numbers.insert(x+1+count,'0')
-print(f'Итоговый массив:\n{new_numbers}')
+n = input('Введите размер массива: ')
+if n.isdigit() == True:
+    numbers = []
+    for i in range(int(n)):
+        numbers.append(random.randint(-100, 100))
+    print(f'Исходный массив:\n{numbers}')
+    new_numbers = numbers.copy()
+    count = 0
+    for x in range(len(numbers)):
+        if numbers[x] < 0:
+            count += 1
+            new_numbers.insert(x+count, '0')
+    print(f'Итоговый массив:\n{new_numbers}')
+else:
+    print('Вместо числа был введен символ. Попробуйте еще раз.')
