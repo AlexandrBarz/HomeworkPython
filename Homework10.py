116841e029d483e7e606c32808a7644b0adfd6d4
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-11-30 18:33:07 +0300

    deleted file Homework10

--- Homework10.py
+++
@@ -1,9 +0,0 @@
-user_input = ''
-while user_input is not int:
-    try:
-        user_input = int(input('Enter a number: '))
-        break
-    except ValueError:
-        print('Please enter a valid number: ')
-
-print('You entered {}'.format(user_input))
