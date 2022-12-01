2ee994e754c2c62b80cbc394116040388e131876
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-12-02 00:46:36 +0300

    solution through functions Homework10

--- Homework10.py
+++ Homework10.py
@@ -8,17 +8,21 @@ def input_list_random(n, lowlimit, highlimit):
     print(f'Сформированный список:\n{list}')
     return list
 
+#  # 1-й способ решения:
+def summa_number(len_list, list_number):
+    summa = 0
+    for i in range(len_list):
+        if (i % 2) == 1:
+            summa += list_number[i]
+    return print(summa)
+
+# 2-й способ решения:
+def sum_number(list_number):
+    return print(sum(list_number[1::2]))
+
 len_list = int(input('Введите кол-во элементов в списке: '))
 lowlimit = int(input('Введите нижнюю границу диапазона списка: '))
 highlimit = int(input('Введите верхнюю границу диапазона списка: '))
 input_list = input_list_random(len_list, lowlimit, highlimit)
-
-#  # 1-й способ решения:
-summa = 0
-for i in range(len_list):
-    if (i % 2) == 1:
-        summa += input_list[i]
-print(summa)
-
-# 2-й способ решения:
-print(sum(input_list[1::2]))
+summa_number(len_list, input_list)
+sum_number(input_list)
