49acbc2a6a5fd1c05acebf22fb4f5cda21504af7
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-12-02 02:17:30 +0300

    corrected file Homework10

--- Homework10.py
+++ Homework10.py
@@ -1,9 +1,21 @@
 # Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 from random import randint
 
+# user_input = ''
+# while user_input is not float:
+#     try:
+#         user_input = float(input('Введите число: '))
+#         break
+#     except ValueError:
+#         print('Ошибка при вводе числа. Необходимо ввести число. ')
+
+# print('Введенное число {}'.format(user_input))
+
+# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+
 def input_list_random(n, lowlimit, highlimit):
     list = []
-    for i in range(n):
+    for _ in range(n):
         list.append(randint(lowlimit, highlimit))
     print(f'Сформированный список:\n{list}')
     return list
@@ -14,11 +26,11 @@ def summa_number(len_list, list_number):
     for i in range(len_list):
         if (i % 2) == 1:
             summa += list_number[i]
-    return print(summa)
+    return print('Сумма элементов списка, стоящих на нечетных позициях: ', summa)
 
 # 2-й способ решения:
 def sum_number(list_number):
-    return print(sum(list_number[1::2]))
+    return print('Сумма элементов списка, стоящих на нечетных позициях: ', sum(list_number[1::2]))
 
 len_list = int(input('Введите кол-во элементов в списке: '))
 lowlimit = int(input('Введите нижнюю границу диапазона списка: '))
