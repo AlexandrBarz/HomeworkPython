ce914040bc69e5388d25c71385a13b379db1a719
AlexandrBarz <113255704+AlexandrBarz@users.noreply.github.com>
2022-12-02 17:08:29 +0300

    Update Homework14.py
    corrected file Homework14

--- Homework14.py
+++ Homework14.py
@@ -1,1 +1,17 @@
 # Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+
+def fibo_num(n):
+    fibo_num =[]
+    fib1, fib2 = 1, 1
+    for i in range(n):
+        fibo_num.append(fib1)
+        fib1, fib2 = fib2, fib1 + fib2
+    fib1, fib2 = 0, 1
+    for i in range(n + 1):
+        fibo_num.insert(0, fib1)
+        fib1, fib2 = fib2, fib1 - fib2
+    return fibo_num
+    
+n = int(input('Введите число: '))
+fibo_nums = fibo_num(n)
+print(f'Список из чисел Фибоначчи для числа {n}:\n{fibo_nums}'
