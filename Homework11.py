33b57805ed3b63f044cdf4dcfebde529edfd33c6
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-12-02 04:31:31 +0300

    corrected file Homework11

--- Homework11.py
+++ Homework11.py
@@ -1,6 +1,5 @@
 # Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 from random import randint
-import math
 
 def input_list_random(n, lowlimit, highlimit):
     list = []
