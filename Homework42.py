1e88688a2cb51684ff55cf464e93aa572def8dd0
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-12-09 18:13:14 +0300

    added file Homework42

--- Homework42.py
+++ Homework42.py
@@ -1,1 +1,8 @@
 # Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности. Не использовать множества.
+def non_repeating_elements(spisok):
+    new_spisok = []
+    [new_spisok.append(i) for i in spisok if i not in new_spisok]
+    return new_spisok
+
+spisok = list(map(int, input('Ввведите числа через пробел: ').split()))
+print(f'Список неповторяющихся элементов последовательности {spisok} ---> {non_repeating_elements(spisok)}')
