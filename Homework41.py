13b21067718b3009ba6e74f3383bacc0a5a51064
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-12-09 17:43:55 +0300

    corrected file Homework41

--- Homework41.py
+++ Homework41.py
@@ -1,32 +1,6 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
-def number_check(input_string: str) -> int:
-    """
-    Функция проверяет корректность ввода
 
-    Agrs:
-    Intput_string  - ввод данных пользователя
-    
-    Return:
-    int - натуральное число
-    """
-    while True:
-        try:
-            number = int(input(input_string))
-            return number
-        except ValueError:
-            print('Некорректный ввод. Попробуйте снова')
-
-def prime_factors(n: int) -> list:
-    """
-    Функция на вход принимает число и выдает список простых множителей
-
-    Args:
-    n  - целое число
-
-    Return:
-    list - список простых множителей введенного числа
-    """
-    i = 2
+def prime_factors(n):
     primfact = []
     while i * i <= n:
         while not n % i:
@@ -37,5 +11,6 @@ def prime_factors(n: int) -> list:
         primfact.append(n)
     return primfact
 
-number = number_check('Введите натуральное число: ')
+# number = number_check('Введите натуральное число: ')
+number = int(input('Введите натуральное число: '))
 print(f'Список простых множителей числа {number}: {prime_factors(number)}')
