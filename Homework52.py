a35d19e3b00e9f444e2fa8fe8b2f25cadeec8917
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-12-13 19:52:39 +0300

    corrected file Homework52

--- Homework52.py
+++ Homework52.py
@@ -4,6 +4,41 @@
 # b) Подумайте как наделить бота ""интеллектом""
 # Если делаете a и b - не нужно создавать отдельных файлов с полностью копированным кодом, лучше выделите в отдельные функции бота и умного бота
 
+# 2 игрока 
+from random import randint
+candies = int(input('Сколько конфет: '))
+take = int(input("Максимальное количество конфет можно взять: "))
+
+def take_to_have_for_first(candies, take):
+    take_candies = candies % (take + 1)
+    if take_candies == 0:
+        take_candies = take
+    return(f'чтобы выиграть первому ходящему необходимо взять {take_candies}')
+first_move = randint(1,2)
+print(f"Первым ходит игрок {first_move}")
+second_move = 3 - first_move
+print(f'Второй ходит игрок {second_move}')
+count = 0
+while candies > 0:
+    count += 1
+    player1 = int(input("Ход первого игрока. Возьмите конфеты: "))
+    candies = candies - player1
+    print(f'осталось конфет: {candies}')
+    print(take_to_have_for_first(candies, take))
+    if candies > 0:
+        player2 = int(input("Ход второго игрока. Возьмите конфеты: "))
+        candies = candies - player2
+        print(f'осталось конфет: {candies}')
+        print(take_to_have_for_first(candies, take))
+if candies == 0:
+    print('Игра окончена')
+if take % 2 != 0:
+    print(f'Победил игрок {first_move}')
+else:
+    print(f'Победил игрок {second_move}')
+
+# против бота
+
 from random import *
 import os
 os.system("cls")
