c40ff672a92e536033c1cab5e7fa1bde509eca4a
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-12-16 18:47:34 +0300

    corrected file Homework66

--- Homework66.py
+++ Homework66.py
@@ -1,1 +1,6 @@
 # Из списка Homework65 оставьте только те пары, где сумма кортежа кратна 5
+import random
+
+numbers = [random.randint(1, 100) for _ in range(20)]
+print(f'Исходный список -> {numbers}')
+print(f'Список пар, где сумма кортежа кратна 5 -> {list(filter(lambda x: (x[0]+x[1])%5==0, enumerate(numbers)))}')
