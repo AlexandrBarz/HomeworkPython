2f2875febc076892eb3be7d5fba231dec5ec3a83
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-12-16 18:57:23 +0300

    corrected file Homework64

--- Homework64.py
+++ Homework64.py
@@ -1,1 +1,7 @@
 # Дан список URL различных сайтов. Нужно составить список доменных имен сайтов.
+url_list = ['https://pythonru.com/osnovy/fajly-v-python-vvod-vyvod',
+            'https://translate.google.ru/',
+            'https://yandex.ru/search'            
+            ]
+domen_list = list(map(lambda i: i[:i.find('/')], [i for i in map(lambda i: i.replace('https://',''), url_list)]))
+print(domen_list)
