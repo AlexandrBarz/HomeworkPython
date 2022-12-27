a70cd7042bf8da9cd19c68d222059ec90267f83c
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-12-27 12:39:22 +0300

    added colorama Homework91

--- Seminar9/Homework91.py
+++ Seminar9/Homework91.py
@@ -1,7 +1,10 @@
 # Создайте программу для игры в ""Крестики-нолики"" при помощи виртуального окружения и PIP
 
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
 
-print('Игра крестики-нолики')
+print(Back.BLUE + 'Игра крестики-нолики' + Style.RESET_ALL)
 
 field = list(range(1,10))
 
@@ -14,20 +17,20 @@ def draw_field(field):
 def take_input(player_token):
    valid = False
    while not valid:
-      player_answer = input("Куда поставим " + player_token+"? ")
+      player_answer = input(Fore.GREEN + "Куда поставим " + Fore.BLUE + player_token + Style.RESET_ALL + Fore.GREEN + "? " + Style.RESET_ALL)
       try:
          player_answer = int(player_answer)
       except:
-         print("Некорректный ввод. Вы уверены, что ввели число?")
+         print(Fore.RED + "Некорректный ввод. Вы уверены, что ввели число?" + Style.RESET_ALL)
          continue
       if player_answer >= 1 and player_answer <= 9:
          if(str(field[player_answer-1]) not in "XO"):
             field[player_answer-1] = player_token
             valid = True
          else:
-            print("Эта клетка уже занята!")
+            print(Fore.YELLOW + "Эта клетка уже занята!" + Style.RESET_ALL)
       else:
-        print("Некорректный ввод. Введите число от 1 до 9.")
+        print(Fore.RED + "Некорректный ввод. Введите число от 1 до 9." + Style.RESET_ALL)
 
 def check_win(field):
    win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
@@ -49,11 +52,11 @@ def main(field):
         if counter > 4:
            tmp = check_win(field)
            if tmp:
-              print(tmp, "Вы выиграли!")
+              print(Fore.CYAN + tmp, Fore.CYAN + "Вы выиграли!" + Style.RESET_ALL)
               win = True
               break
         if counter == 9:
-            print("Ничья!")
+            print(Fore.YELLOW + "Ничья!" + Style.RESET_ALL)
             break
     draw_field(field)
 main(field)
